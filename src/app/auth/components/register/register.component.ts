b929161d4ab24a3c1b705b4ed4b7947a5ea6f77a
Luka Pendelj <pendeljluka@gmail.com>
2024-12-13 23:53:56 +0100

    Finally updated properly

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -6,7 +6,8 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-register',
   imports: [RouterLink, CommonModule, ReactiveFormsModule],
-  templateUrl: './register.component.html'
+  templateUrl: './register.component.html',
+  standalone: true
 })
 export class RegisterComponent implements OnInit {
 
